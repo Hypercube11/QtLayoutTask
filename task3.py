c111e775a4c1abce3047ef33b230cd00334c0723
Hypercube11 <132039714+Hypercube11@users.noreply.github.com>
2023-05-18 19:45:18 +1100

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,33 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+app = QApplication([]) 
+main_win = QWidget()
+main_win.show()
+main_win.setWindowTitle('Easy Editor')
+main_win.resize(400, 200)
 
+v_main = QVBoxLayout()
+main_win.setLayout(v_main)
+
+h_main = QHBoxLayout()
+v_main.addLayout(h_main)
+
+h_main_2 = QHBoxLayout()
+v_main.addLayout(h_main_2)
+
+b1 = QPushButton('Button 1')
+h_main.addWidget(b1)
+
+b2 = QPushButton("Button 2")
+h_main.addWidget(b2)
+
+b3 = QPushButton("Button 3")
+h_main.addWidget(b3) 
+
+b4 = QPushButton("Button 4")
+h_main_2.addWidget(b4)
+
+b5 = QPushButton('Button 5')
+h_main_2.addWidget(b5)
+
+app.exec_()
